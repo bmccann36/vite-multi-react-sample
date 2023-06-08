15367f0f6323051cd09783fbbff499ca41ae3891
brianmccann <bmccann36@gmail.com>
2023-06-08 12:13:09 -0400

    remove commented out code from index.jsx

--- client-legacy/src/index.jsx
+++ client-legacy/src/index.jsx
@@ -2,7 +2,3 @@ export * from './createLegacyRoot.jsx'
 export * from './Greeting.jsx'
 export * from './createLegacyRoot.jsx'
 
-
-// export const myFunc = () => {
-//   console.log('it seems it does import as jsx')
-// }
